f12a77f2e9002164fa03d837ba4aba2dac484cd8
Bruno Juan <bruno.kirovs@gmail.com>
2025-06-08 16:02:58 -0300

    update na implementação do darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { About } from "./pages/About.tsx";
 import { Projects } from "./pages/Projects.tsx";
 import { ThemeToggle } from "./components/ThemeToggle.ts";
 
+//<div className="bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text min-h-screen flex items-center justify-center gap-4">
+      //  <ThemeToggle />
+      //</div>*/
+
 function App() {
   return (
     <div className="flex flex-col">
-      <div className="bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text min-h-screen flex items-center justify-center gap-4">
-        <ThemeToggle />
-      </div>
+
       <Header />
       <div className="flex-grow px-4 pt-24">
         <Routes>

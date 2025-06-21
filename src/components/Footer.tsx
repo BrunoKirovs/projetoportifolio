c62b22c363f670a0159a6b8a43c592404b523150
Bruno Juan <bruno.kirovs@gmail.com>
2025-06-21 14:37:30 -0300

    Mudançãs Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,27 @@
+import { Container, Row, Col, Stack, } from "react-bootstrap";
+
 export function Footer() {
   return (
-    <footer className="bg-gray-900 text-white p-4 text-center mt-8">
-      <p>
-        @{new Date().getFullYear()} Bruno Juan. Todos os direitos pertencem aos
-        meus orientadores
+    <footer className="bg-gray-900 text-white p-4 mt-8">
+      <Container>
+        <Row className="bg-primary text-white p-4 ">
+            <Col className="mx-4">
+              <Stack>
+                <h2>Sony Blame</h2>
+                <p>Sony blame tag</p>
+              </Stack>
+            </Col>
+            <Col>
+                <h4>Contato</h4>
+                <p>bruno.kirovs@gmail.com</p>
+            </Col>
+        </Row>
+      </Container>
+      <p className="text-center">
+        @{new Date().getFullYear()} Deesenvolvido por Bruno Juan - Enginner Developer.
       </p>
     </footer>
   );
 }
+
+

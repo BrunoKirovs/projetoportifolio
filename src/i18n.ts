37a9802f4f340f9e263cd622a63ba58bbf433f74
Bruno Juan <bruno.kirovs@gmail.com>
2025-06-23 23:54:02 -0300

    comentando arquivo i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,9 @@
-import i18n from 'i18next';
+/*import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './locales/en/translation.json';
 import pt from './locales/pt/transalation.json';
 
-i18n.use(initReactI18next).init({
+/*i18n.use(initReactI18next).init({
     resources:{
         en:{ translation: en},
         pt:{ translation: pt},
@@ -15,4 +15,4 @@ i18n.use(initReactI18next).init({
     }
 })
 
-export default i18n;
+export default i18n;*/
